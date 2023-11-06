d3bca07d049f8b23f381ddc516b03ce95a9c1ed5
Black candy <122829729+blackcandy-54@users.noreply.github.com>
2023-11-06 13:01:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+const gettUrl = window.location.href;
+const Url = gettUrl.replace("/stream/", "/");
+
+function playInVLC() {
+    const vlcUrl = `vlc://${Url}`;
+    window.location.href = vlcUrl;
+}
