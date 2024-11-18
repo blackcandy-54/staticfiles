1d835712de829b1e6c81df93291c546740dce4eb
VIXYZ <71164943+mrvigneshvt@users.noreply.github.com>
2024-11-18 20:32:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 const gettUrl = window.location.href;
 const Url = gettUrl.replace("/stream/", "/");
 
-function copyURL() {
-navigator.clipboard.writeText(Url)
-        .then(function() {
-            // Provide feedback when the URL is copied successfully
-            const feedback = document.getElementById("feedback");
-            feedback.textContent = "URL copied to clipboard!";
-            feedback.style.color = "green";
 
-            // Show an alert confirming the URL was copied
-            alert("Link copied to clipboard!");
-        })
-        .catch(function(err) {
-            // Handle errors in case copying fails
-            const feedback = document.getElementById("feedback");
-            feedback.textContent = "Failed to copy URL!";
-            feedback.style.color = "red";
-            console.error("Error copying text: ", err);
-        });
+function copyURL() {
+  navigator.clipboard.writeText(Url)
+    .then(() => {
+      // Success
+      const feedback = document.getElementById("feedback");
+      feedback.textContent = "URL copied to clipboard!";
+      feedback.style.color = "green";
+      alert("Link copied to clipboard!");
+    })
+    .catch(err => {
+      // Fallback for older browsers
+      if (!navigator.clipboard || !navigator.clipboard.writeText) {
+        const textArea = document.createElement("textarea");
+        textArea.value = Url;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');   
+
+        textArea.remove();
+        const feedback = document.getElementById("feedback");
+        feedback.textContent = "URL (hopefully) copied to clipboard!";
+        feedback.style.color = "green";
+      } else {
+        // Log error for non-compatibility issues
+        console.error("Error copying text:", err);
+        const feedback = document.getElementById("feedback");
+        feedback.textContent = "Failed to copy URL!";
+        feedback.style.color = "red";
+      }
+    });
 }
 
 function playInVLC() {
